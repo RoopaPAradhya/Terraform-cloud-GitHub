489ad479d0fb59638d06e0cfa42a0f8cbccd528f
RoopaPAradhya <121972163+RoopaPAradhya@users.noreply.github.com>
2023-03-09 01:52:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
         #profile = "default"
 	}
 
-resource "aws_instance" "Raminfra" {
+resource "aws_instance" "Roopainfra" {
 	ami = "ami-006dcf34c09e50022" 
 	instance_type = "t2.micro"
 }
